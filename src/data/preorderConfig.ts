2623afec9b75cf6befd7793d3ca8af8d605e677a
YoshiNagev <josiassaas2003@gmail.com>
2026-09-07 12:21:22 +0200

    kurzer Shop Test

--- src/data/preorderConfig.ts
+++ src/data/preorderConfig.ts
@@ -4,7 +4,7 @@ export const preorderConfig = {
   shippingCost: 3,
   allowedQuantities: [1, 2, 3, 4],
   estimatedShippingDate: "Dezember 2026",
-  preorderOpen: false,
+  preorderOpen: true,
   contactEmail: "kontakt@dasveganequartett.de",
 } as const;
 
