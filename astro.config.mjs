dea987e3a550cd6c6d4523af012fcff040981f27
YoshiNagev <josiassaas2003@gmail.com>
2026-09-11 21:29:59 +0200

    Add sitemap and SEO configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://dasveganequartett.de",
   output: "server",
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
